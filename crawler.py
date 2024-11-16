5638dd9aaf987f08442f26f60b555b0691fa39ad
Carlos <carcast1900@gmail.com>
2024-11-15 17:56:57 -0800

    Initial commit

---
+++ crawler.py
@@ -0,0 +1,7 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from bs4 import BeautifulSoup
+
+#def crawlerThread (frontier):
+	#while(frontier.)
